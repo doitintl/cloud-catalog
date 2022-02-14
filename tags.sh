eaac18746e597d2c7c6f310639afe905794fd3be
Alexei Ledenev <alexei.led@gmail.com>
2022-02-14 19:36:18 +0200

    add gsuite and ms365

--- tags.sh
+++ tags.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-jq -r '.[] | .tags ' data/aws.json data/gcp.json data/cmp.json | jq -s 'add | unique | sort' > data/tags.json
+jq -r '.[] | .tags ' data/aws.json data/gcp.json data/cmp.json data/gsuite.json data/ms365.json | jq -s 'add | unique | sort' > data/tags.json
