efb4dbf565de55071c767ff547ef547528ce1f60
Bernhard Weisshuhn (a.k.a. bernhorst) <bkw@codingforce.com>
2022-08-29 09:15:30 +0200

    feat: add aws category `other`

--- aws.sh
+++ aws.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+OTHER_JSON=$(cat <<'EOF'
+{ "id": "aws/other", "name": "Other",
+  "summary": "Other AWS services.",
+  "url": "",
+  "categories": [
+    {
+      "id": "other",
+      "name": "Other"
+    }
+  ],
+  "tags": ["aws/platform"]
+}
+EOF
+)
 
 echo "listing AWS services"
 
 curl -s 'https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&sort_by=item.additionalFields.productCategory&sort_order=asc&size=500&item.locale=en_US&tags.id=!aws-products%23type%23feature&tags.id=!aws-products%23type%23variant' \
   | jq -r '.items[] | {"id": "aws/\(.item.name)", "name": .item.additionalFields.productName, "summary": .item.additionalFields.productSummary, "url": .item.additionalFields.productUrl, "categories": [.tags[] | select(.tagNamespaceId=="GLOBAL#tech-category") | {"id": (.name | gsub(" "; "-") | gsub("&"; "and") | ascii_downcase), "name": .description}], "tags": ["aws/platform", "aws/service/\(.item.name)", "aws/category/\(.tags[] | select(.tagNamespaceId=="GLOBAL#tech-category") | .name | gsub(" "; "-") | gsub("&"; "and") | ascii_downcase)"] }' \
   | jq -n '. |= [inputs]' \
+  | jq -r ". += [$OTHER_JSON]" \
   | jq -r 'sort_by(.id)' > data/aws.json
 
 # fix wrong category descriptions
