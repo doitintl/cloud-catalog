c47f5c67488e0600575d7cabf41f623943550d37
Alexei Ledenev <alexei.led@gmail.com>
2022-09-12 17:29:36 +0300

    add full url if nissing

--- gcp.sh
+++ gcp.sh
@@ -6,7 +6,20 @@ echo "listing GCP services"
 
 curl -s 'https://cloud.google.com/products/' \
   | pup 'a.cws-card json{}' \
-  | jq -r '.[] | {"id": ("gcp/" + ."track-name" | gsub(" ";"-")), "name": .children[0].children[0].children[0].text, "summary": .children[0].children[0].children[1].text, "url": .href, "categories": [{"id": (."track-metadata-module_headline" | gsub(" ";"-")), "name": ."track-metadata-module_headline"}]}' \
+  | jq -r '.[]
+          | {
+              "id": ("gcp/" + ."track-name" | gsub(" ";"-")),
+              "name": .children[0].children[0].children[0].text,
+              "summary": .children[0].children[0].children[1].text,
+              "url": (if .href | startswith("https://cloud.google.com/") then .href else "https://cloud.google.com" + .href end),
+              "categories":
+              [
+                {
+                  "id": (."track-metadata-module_headline" | gsub(" ";"-")),
+                  "name": ."track-metadata-module_headline"
+                }
+              ]
+            }' \
   | jq -n '. |= [inputs]' \
   | jq '. | group_by(.id) | map(.[] + {("categories"): map(.categories) | add}) | unique_by(.id)' \
   | jq '.[] 
