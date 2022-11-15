1d2c66961314b74afb85f7e0c6aa5df49b1df877
Tal Cohen <taltultc@yahoo.com>
2022-11-15 15:09:20 +0200

    fix(aws): remove <p>

--- aws.sh
+++ aws.sh
@@ -42,8 +42,8 @@ curl -s 'https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-produ
 sed -i.bak 's/<p>containers<\/p>\\r\\n/Containers/g' data/aws.json
 sed -i.bak 's/<p>media-services<\/p>\\r\\n/Media Services/g' data/aws.json
 sed -i.bak 's/<p>Quantum Technologies<\/p>\\r\\n/Quantum Technologies/g' data/aws.json
-sed -i.bak 's/<p>Empower your organization with Alexa<\/p>\\r\\n/Empower your organization with Alexa/g' data/aws.json
-sed -i.bak 's/<p>Build mobile &amp; web apps without programming<\/p>\\r\\n/Build mobile web apps without programming/g' data/aws.json
+sed -i.bak 's/<p>//g' data/aws.json
+sed -i.bak 's/&amp;//g' data/aws.json
 
 # cleanup url
 sed -i.bak 's/\/?did=ap_card&trk=ap_card//g' data/aws.json
