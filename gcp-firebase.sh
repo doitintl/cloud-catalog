9b7eec4a28b8f3e7d1746ac1db3ecff081a5f52d
Rick Boss <rickbau5@users.noreply.github.com>
2023-02-02 11:02:31 -0800

    feat: scrape firebase services

---
+++ gcp-firebase.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -e
+
+echo "listing Firebase services"
+
+pages=(
+  https://firebase.google.com/products-build
+  https://firebase.google.com/products-release
+  https://firebase.google.com/products-engage
+)
+
+for url in "${pages[@]}"; do
+  output="data/gcp-firebase-${url##*/}.json"
+  curl -s $url \
+    | pup 'div.product-grid__content json{}' \
+    | jq -r '.[]
+            | {
+                "id": ("gcp/firebase/" + .children[0].children[0].id | gsub(" ";"-")),
+                "name": .children[0].children[0].children[0].text,
+                "summary": .children[1].children[0].text,
+                "url": ( 
+                  if (.children[0].children[0].children[0].href | startswith("https://") ) then 
+                    .children[0].children[0].children[0].href
+                  else 
+                    "https://firebase.google.com" + .children[0].children[0].children[0].href 
+                  end
+                ),
+                "categories": [
+                  {
+                    "id": "firebase",
+                    "name": "Firebase"
+                  }
+                ],
+                "tags": [
+                  "gcp/platform",
+                  "gcp/firebase",
+                  (
+                    "gcp/firebase/" + (
+                      .children[0].children[0].id
+                        | gsub(" ";"-")
+                        | gsub("firebase";"")
+                        | sub("^-";"")
+                    )
+                  )
+                ]
+              }' \
+    | jq -n '. |= [inputs]' \
+    > "$output"
+done
+
+find data -name "gcp-firebase-*.json" \
+  | xargs -n 1 bash -c 'jq ".[]" $1' _ \
+  | jq -s 'unique_by(.id)' \
+  | jq 'sort_by(.id)' \
+  > data/gcp-firebase.json
+
+echo "done"
--- tags.sh
+++ tags.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-jq -r '.[] | .tags ' data/aws.json data/gcp.json data/cmp.json data/gsuite.json data/ms365.json data/azure.json data/finance.json data/credits.json | jq -s 'add | unique | sort' > data/tags.json
+jq -r '.[] | .tags ' data/aws.json data/gcp.json data/cmp.json data/gsuite.json data/ms365.json data/azure.json data/finance.json data/credits.json data/gcp-firebase.json | jq -s 'add | unique | sort' > data/tags.json
