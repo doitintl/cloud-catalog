ddb69488d10ffdaba51068f84fbde1047f8ef794
Rick Boss <rickbau5@users.noreply.github.com>
2023-02-08 09:13:47 -0800

    fix: remove slash from id

--- gcp-firebase.sh
+++ gcp-firebase.sh
@@ -11,10 +11,10 @@ pages=(
 )
 
 printf "%s\n" "${pages[@]}" \
-    | xargs -n 1 bash -c 'curl -s $1 | pup "div.product-grid__content json{}"' _ \
+    | xargs -t -n 1 bash -c 'curl -s $1 | pup "div.product-grid__content json{}"' _ \
     | jq -r '.[]
              | {
-                  "id": ("gcp/firebase/" + .children[0].children[0].id | gsub(" ";"-")),
+                  "id": ("gcp/firebase-" + .children[0].children[0].id | gsub(" ";"-")),
                   "name": .children[0].children[0].children[0].text,
                   "summary": .children[1].children[0].text,
                   "url": (
