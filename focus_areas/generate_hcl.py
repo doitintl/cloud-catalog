0c6064ac16a21b42d934dec262b822e36f654e8f
Adam North <anorth848@gmail.com>
2024-05-21 17:31:22 -0400

    update to generate_hcl

--- focus_areas/generate_hcl.py
+++ focus_areas/generate_hcl.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def main():
     hcl_dict = {}
@@ -7,9 +8,15 @@ def main():
         focus_areas = json.load(f)
         for i in focus_areas:
             node = i['id'].replace('/', '::')
+            if len(i['secondary_skills']) == 0:
+                del(i['secondary_skills'])
+
             hcl_dict[node] = i
 
-        print(json.dumps(hcl_dict, indent=2, separators=[",", " = "]))
+        json_string = json.dumps(hcl_dict, indent=2, separators=[",", "\t= "])
+
+        hcl_string = re.sub('"(id|practice_area|name|primary_skills|secondary_skills)"', '\\1', json_string)
+        print(hcl_string)
 
 
 if __name__ == '__main__':
